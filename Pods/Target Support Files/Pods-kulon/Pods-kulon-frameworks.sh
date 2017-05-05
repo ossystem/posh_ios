17d0fbcfe606512bc58f37cf75dfa6ef174ef04d
Artemiy <temitrix@gmail.com>
2017-05-05 15:17:07 +0300

    pod update

--- Pods/Target Support Files/Pods-kulon/Pods-kulon-frameworks.sh
+++ Pods/Target Support Files/Pods-kulon/Pods-kulon-frameworks.sh
@@ -84,12 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DynamicBlurView/DynamicBlurView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxBluetoothKit/RxBluetoothKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DynamicBlurView/DynamicBlurView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxBluetoothKit/RxBluetoothKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
